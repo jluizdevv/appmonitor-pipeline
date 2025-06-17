e1fbd13d692a1628dbd99f9599a471a9b4cc4575
José <jluizbc94@gmail.com>
2025-06-17 18:40:48 -0300

    primeiro commit

---
+++ src/test/java/appmonitor/AppMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package appmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
